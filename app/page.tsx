c96971d7987f2252e46a7636dac718d16fde390d
khashayaryaghma <khashayaryaghma@gmail.com>
2023-12-15 20:55:18 +0330

    edit page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Form from "@/components/Form";
+import { Tasks } from "@/components/Tasks";
+import { getData } from "@/utils/dataservices";
 
-
-export default function Home() {
+export default async function Home() {
+  const tasks = await getData("http://localhost:8000/tasks")
   return (
     <main >
       <Form/>
+      <Tasks data={tasks} />
     </main>
   )
 }
